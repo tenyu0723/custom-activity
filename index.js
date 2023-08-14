343340cc615a3bdb2ab788ccadffb42ae72f57e9
tenyu0723 <90138515+tenyu0723@users.noreply.github.com>
2023-08-14 16:16:06 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const { Client,CustomStatus } = require("discord.js-selfbot-v13");
+const {Client,RichPresence} = require("discord.js-selfbot-v13");
 const http = require("http");
+const cron = require('node-cron');
 const client = new Client({
   syncStatus: false,
   checkUpdate: false,
 });
-
-const change_interval = 4 //変更する間隔(秒) 4以上推奨
-const words = ["1","2","3","4"] //ステータスのワード(左から変更されて右までいったら一番左になります)
 
 http.createServer(async function(request, response) {
     response.writeHead(200, { 'Content-Type': 'text/plain;charset=utf-8' })
@@ -21,14 +19,16 @@ if (process.env.TOKEN == undefined) {
 
 client.on("ready", () => {
   console.log(`${client.user.tag} is ready!`)
-  let i = 0
-  setInterval(() => {
-    client.settings.setCustomStatus({ text: words[i] })
-    i++
-    if(i > words.length-1){
-      i=0
-    }
-  },1000*change_interval)
+  const r = new RichPresence()
+  .setApplicationId('817229550684471297')  //いじらないでね
+  .setType('PLAYING') // PLAYING:プレイ中 WATCHING:視聴中 STREAMING:配信中
+	.setName('Name') //アクティビティの名前(タイトル)
+	.setDetails('Detail') //アクティビティの説明(長すぎない方がいい)
+	.setStartTimestamp(Date.now())  //いじらないでね
+	.setAssetsLargeImage('mp:attachments/1138762211465117727/1140543857679937687/McVkTazCaveLwqoDuX_E7ayTgdK4DPbrKCGcPSIZT4b783j6HJvJYu0uxQAuzCw7BCs.png') //画像の設定方法はreadme.mdを参照
+	.setAssetsLargeText('Image txt') //画像に込める文字
+	.addButton('ボタンの表示テキスト', '押したときのリンク') //ボタンを追加する処理(コピペすれば複数可能)
+  client.user.setActivity(r);
 });
 
 client.login(process.env.TOKEN)
